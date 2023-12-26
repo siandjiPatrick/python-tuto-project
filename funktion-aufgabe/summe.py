51c8ce35abc829975378e12145cab6b05efba71c
patricksiandji <patrick.djumedio@pcg.io>
2023-12-26 10:38:29 +0100

    add execption

--- funktion-aufgabe/summe.py
+++ funktion-aufgabe/summe.py
@@ -7,7 +7,7 @@ def summe(n:int, m:int):
     for k in range(n, (m+1)+1):
 
         res = 2**(2*k)
-        print("pour k = ", k , "--> 2^2*",k, "= ",res)
+        print("fuer k = ", k , "--> 2^2*",k, "= ",res)
 
         # Fügen wir das Ergebnis in die Liste ein
         list_result.append(res)
@@ -16,11 +16,28 @@ def summe(n:int, m:int):
 
 ############### hauptprogramm ##################
 
-print("\n!!! n und m müssen ganze zahl sein ")
 
-# Benutzer Interaktion
-n = int(input("geben Sie n ein -->  "))  # 
-m = int(input("geben Sie m ein -->  "))
+
+
+error_input = True
+while error_input:
+    try :
+        print("\nGeben Sie n und m ein(n und m müssen ganze Zahl sein): ")
+        # Benutzer Interaktion
+        n = int(input("geben Sie n ein -->  "))  # 
+        m = int(input("geben Sie m ein -->  "))
+        print(type(n))
+        print(type(n))
+
+    except KeyboardInterrupt:
+        print("\nprogramm wurde beendet")
+        exit()
+    except:
+        print("\n!!! Eingabe falsch ")
+    else:
+        error_input = False
+        print("else")
+
 
 result = summe(n,m)
 
